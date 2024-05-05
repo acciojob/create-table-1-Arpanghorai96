1c09340ca4bb00926fd3263823b5654b4d7fa622
Arpanghorai96 <121602804+Arpanghorai96@users.noreply.github.com>
2024-05-05 02:38:33 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	const table=document.getElementById("sampleTable");
+    var tr=table.insertRow(0);
+    var td1=tr.insertCell(0)
+    var td2=tr.insertCell(1)
+    td1.innerText="New Cell"
+    td2.innerText="New Cell2"
+	
 }
